f53be5dfb0d430f8dfaf962a4fa4fbab5e55dca0
严春光 <842997290@qq.com>
2017-07-24 12:07:32 +0800

    请求权限

--- MyLook/app/src/main/java/com/qianqi/mylook/activity/MainActivity.java
+++ MyLook/app/src/main/java/com/qianqi/mylook/activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.qianqi.mylook.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -79,6 +85,8 @@ public class MainActivity extends BaseActivity{
 //        waveView.cancelAnimation();
         waveView.startAnimation();
         EventBus.getDefault().register(this);
+
+        requestPermission();
     }
 
     @Override
@@ -196,5 +204,71 @@ public class MainActivity extends BaseActivity{
         if(minute > 0)
             s += minute + getString(R.string.minute);
         return s;
+    }
+
+    public final static int REQUEST_READ_PHONE_STATE = 1;
+    public final static int REQUEST_READ_CALL_LOG = 2;
+    public final static int REQUEST_ACCESS_COARSE_LOCATION = 3;
+    public final static int REQUEST_ACCESS_FINE_LOCATION = 4;
+    public final static int REQUEST_READ_EXTERNAL_STORAGE = 5;
+    public final static int REQUEST_WRITE_EXTERNAL_STORAGE = 6;
+    private void requestPermission()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            int checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STATE);
+                return;
+            }
+
+            checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALL_LOG}, REQUEST_READ_CALL_LOG);
+                return;
+            }
+
+            checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_ACCESS_COARSE_LOCATION);
+                return;
+            }
+
+            checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+                return;
+            }
+
+            checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE);
+                return;
+            }
+
+            checkCallPhonePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            default:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(this,"成功获取权限",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(this,"获取权限失败",Toast.LENGTH_SHORT).show();
+                }
+                requestPermission();
+                break;
+        }
+
     }
 }
