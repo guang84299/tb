eb2efa87c0c88581afdd70b32300ca2e9a35ce10
严春光 <842997290@qq.com>
2017-08-09 19:45:51 +0800

    去掉权限申请判断

--- MyLook/app/src/main/java/com/qianqi/mylook/core/CoreReceiver.java
+++ MyLook/app/src/main/java/com/qianqi/mylook/core/CoreReceiver.java
@@ -1,25 +1,14 @@
 package com.qianqi.mylook.core;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.provider.Settings;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.WindowManager;
 
 import com.qianqi.mylook.BusTag;
 import com.qianqi.mylook.MainApplication;
-import com.qianqi.mylook.activity.GPermissionActivity;
-import com.qianqi.mylook.activity.MainActivity;
 import com.qianqi.mylook.model.PackageModel;
-import com.qianqi.mylook.stat.StatActivity;
 import com.qianqi.mylook.utils.L;
 
 import org.greenrobot.eventbus.EventBus;
@@ -35,42 +24,6 @@ public class CoreReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         L.d("CoreReceiver:onReceive");
-        int checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED)
-        {
-            showAlert();
-            return;
-        }
-        checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CALL_LOG);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-            showAlert();
-            return;
-        }
-
-        checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-            showAlert();
-            return;
-        }
-
-        checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-            showAlert();
-            return;
-        }
-
-        checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-            showAlert();
-            return;
-        }
-
-        checkCallPhonePermission = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (checkCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
-            showAlert();
-            return;
-        }
-
         if(intent.getAction().equals("android.hardware.usb.action.USB_STATE"))
         {
             boolean connected = intent.getExtras().getBoolean("connected");
@@ -102,58 +55,5 @@ public class CoreReceiver extends BroadcastReceiver {
         if(intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)){
             EventBus.getDefault().post(new BusTag(BusTag.TAG_NETWORK_CHANGED));
         }
-    }
-
-    private void showAlert()
-    {
-        if(isShowAlert)
-            return;
-        isShowAlert = true;
-        int num = MainApplication.getInstance().getSharedPreferences("permissnum", Activity.MODE_PRIVATE).getInt("num",0);
-        if(num<20)
-        {
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainApplication.getInstance());
-            dialogBuilder.setTitle("Error!");
-            dialogBuilder.setMessage("TurboBattery has no permissions and can not run.");
-            dialogBuilder.setCancelable(false);
-            dialogBuilder.setPositiveButton("I know", new DialogInterface.OnClickListener() {//添加确定按钮
-                @Override
-                public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
-                    isShowAlert = false;
-                }
-            });
-            AlertDialog alertDialog = dialogBuilder.create();
-            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-            alertDialog.show();
-        }
-        else
-        {
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainApplication.getInstance());
-            dialogBuilder.setTitle("Error!");
-            dialogBuilder.setMessage("TurboBattery has no permissions and can not run.");
-            dialogBuilder.setCancelable(false);
-            dialogBuilder.setPositiveButton("Set", new DialogInterface.OnClickListener() {//添加确定按钮
-                @Override
-                public void onClick(DialogInterface dialog, int which) {//确定按钮的响应事件
-                    isShowAlert = false;
-                    openPermissionActivity();
-//                    Intent in = new Intent();
-//                    in.setAction("com.qianqi.openpermission");
-//                    MainApplication.getInstance().sendBroadcast(in);
-                }
-            });
-            AlertDialog alertDialog = dialogBuilder.create();
-            alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-            alertDialog.show();
-        }
-        MainApplication.getInstance().getSharedPreferences("permissnum", Activity.MODE_PRIVATE).edit().putInt("num",num+1).commit();
-    }
-
-    private void openPermissionActivity()
-    {
-        Intent intent = new Intent();
-        intent.setClass(MainApplication.getInstance(),GPermissionActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        MainApplication.getInstance().startActivity(intent);
     }
 }
