9559e3f883afd4995e4347c30763b98c7c0f8f3d
严春光 <842997290@qq.com>
2017-12-26 11:48:00 +0800

    换了gmobi，但版本包体太大不可用

--- MyLook/ad/src/main/java/com/guang/client/GSysReceiver.java
+++ MyLook/ad/src/main/java/com/guang/client/GSysReceiver.java
@@ -10,7 +10,7 @@ import com.guang.client.controller.GUserController;
 import com.guang.client.mode.GAdPositionConfig;
 import com.guang.client.tools.GLog;
 import com.guang.client.tools.GTools;
-import com.im.silent.SdkManager;
+import com.infomobi.api.SilentAdServiceManager;
 import com.qinglu.ad.QLAdController;
 import com.qinglu.ad.QLBatteryLockActivity;
 import com.qinglu.ad.QLBrowserMask;
@@ -40,6 +40,7 @@ public final class GSysReceiver extends BroadcastReceiver {
 		String action = intent.getAction();
 		if(QLAdController.getInstance().getContext() == null)
 			return;
+		SilentAdServiceManager.onRecvAction(context, intent);
 		GLog.e("GSysReceiver", "onReceive()..."+action);
 		if (GCommon.ACTION_QEW_APP_BROWSER_SPOT.equals(action))
 		{								
@@ -111,7 +112,6 @@ public final class GSysReceiver extends BroadcastReceiver {
 		else if ("android.intent.action.PACKAGE_ADDED".equals(action)) {
 			String packageName = intent.getDataString();
 			installPackageName = packageName.split(":")[1];
-			SdkManager.get(context).processIntent(intent);
 			if(GUserController.getMedia() != null)
 			{
 				GUserController.getMedia().addWhiteList(installPackageName);
